54d5c1aa23643b6c926750f69a12d83ddaf4ff7d
James <ninjabobtwo@gmail.com>
2022-04-13 22:41:50 -0500

    a little organization

--- ARRPG/Assets/Scripts/SelectionController.cs
+++ ARRPG/Assets/Scripts/SelectionController.cs
@@ -8,6 +8,7 @@ public class SelectionController : MonoBehaviour
     [SerializeField] private Camera arCam;
     private Vector2 touchPos = default;
 
+
     // Start is called before the first frame update
     void Start()
     {
